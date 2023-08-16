85cfa5f1dfa8260b8102f5453016497549830aae
Nehcnud <nenaddjukin@gmail.com>
2023-08-16 16:15:55 +0200

    added routes

---
+++ webshop/src/pages/shop/shop.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Shop = () => {
+  return(
+  <div className="shop">
+    <div className="shopTitle">
+      <h1>Shop Name</h1>
+    </div>
+      <div className="shopItems">
+      <div/>  
+  </div>
+  )
+};
